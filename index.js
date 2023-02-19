62d78794e0411cee34ea639466b2776de790481d
Geoff Kelly <geoffkellync@gmail.com>
2023-02-19 12:27:30 -0500

    finished MVP

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ const port = process.env.PORT || 9000
 
 
 server.listen(port, () => {
-    console.log(`*** Server Running on http://localhost:${port} ***`);
+    console.log(`*** Server Running on http://localhost:${port} ***`); 
     });
 
